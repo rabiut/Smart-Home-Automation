c93cd04cce4af323bff5d002d1662dc363c5a2bf
rabiut <90636484+rabiut@users.noreply.github.com>
2023-06-14 18:24:49 -0400

    new bulbs

---
+++ website/APIs/wiz/outside_bulbsOFF.py
@@ -0,0 +1,16 @@
+from pywizlight import wizlight, PilotBuilder, discovery
+import asyncio
+
+async def main():
+    # Replace with your bulbs' IP addresses
+    IP1 = "192.168.2.56"
+    IP2 = "192.168.2.57"
+    bulb1 = wizlight(IP1)
+    bulb2 = wizlight(IP2)
+    
+    # Turn off the bulbs
+    await bulb1.turn_off()
+    await bulb2.turn_off()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ website/APIs/wiz/outside_bulbsON.py
@@ -0,0 +1,23 @@
+from pywizlight import wizlight, PilotBuilder, discovery
+import asyncio
+
+async def main():
+    IP1 = "192.168.2.56"
+    IP2 = "192.168.2.57"
+    bulb1 = wizlight(IP1)
+    bulb2 = wizlight(IP2)
+
+
+    # Set brightness to maximum
+    pilot1 = PilotBuilder(brightness=255)
+    pilot2 = PilotBuilder(brightness=255)
+    
+    # Turn on the bulbs
+    await bulb1.turn_on(pilot1)
+    await bulb2.turn_on(pilot2)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    
+    
